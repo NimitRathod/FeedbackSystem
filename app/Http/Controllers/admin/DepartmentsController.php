56661958a00fc77dc8eb8a29dd484e8fcf75c7d1
NimitRathod <nimitrathod1997@gmail.com>
2018-11-20 11:37:02 +0530

    Department Form Create

--- app/Http/Controllers/admin/DepartmentsController.php
+++ app/Http/Controllers/admin/DepartmentsController.php
@@ -35,7 +35,8 @@ class DepartmentsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        echo $request->all();
+        exit();
     }
 
     /**
